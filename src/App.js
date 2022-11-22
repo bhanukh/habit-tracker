f09725e41f5feb1394ff244701b9bacd30ba1332
bhanukh <brkhandelwal1997@gmail.com>
2022-11-22 15:08:29 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './Components/Navbar'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useSelector } from "react-redux";
+import AddHabit from './Components/Addhabit';
+import Week from './Components/Week';
+import Lists from './Components/Lists';
+
 
 function App() {
+  let habits=useSelector((state)=>state["habits"])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <Navbar />
+   <Routes>
+     <Route path='/' element={<Lists habits={habits} />} />
+     <Route path="/add-habit" element={<AddHabit/>}/>
+     <Route path="/week-view" element={<Week/>}/>
+   </Routes>
+   <ToastContainer />
     </div>
   );
 }
